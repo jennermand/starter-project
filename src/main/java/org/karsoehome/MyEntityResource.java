04974aa4600184de2726f596328d0d7f89c6a1e9
quarkusio <no-reply@quarkus.io>
2024-06-20 07:01:48 +0000

    Initial commit

---
+++ src/main/java/org/karsoehome/MyEntityResource.java
@@ -0,0 +1,6 @@
+package org.karsoehome;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+
+public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
+}
